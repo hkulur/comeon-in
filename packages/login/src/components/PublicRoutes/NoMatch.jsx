cc8ed913e27d16562b36a335753aa4d000ae28fa
Fazal <syed.fazal@jifflenow.com>
2020-03-09 03:11:58 +0530

    Fazal : Minor fix

--- packages/login/src/components/PublicRoutes/NoMatch.jsx
+++ packages/login/src/components/PublicRoutes/NoMatch.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Button } from '@comeonin/design-system/lib/button'
 import { Paragraph2, H4 } from '@comeonin/design-system/lib/typography'
@@ -7,18 +8,25 @@ import { Root, Center, Header, StyledLink } from 'components/StyledComponents'
 import NoMatchIcon from 'components/Icons/NoMatch'
 
 const NoMatch = () => {
+  const user = useSelector(state => state.user)
+  const notLoggedIn = Object.keys(user).length === 0
+
   return (
     <Root>
       <Center>
         <Header>
           <NoMatchIcon size={64} />
           <H4>No Page Found</H4>
+          {
+            notLoggedIn
+            ? (
+              <StyledLink
+                data-baseweb='link'
+                to='/login'
+              >Login</StyledLink>
+            ) : null
 
-          <StyledLink
-            data-baseweb='link'
-            to='/login'
-          >Login</StyledLink>
-          
+          }
         </Header>
       </Center>
     </Root>
